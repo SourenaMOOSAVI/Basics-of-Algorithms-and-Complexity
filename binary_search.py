684c5067c79b9559e24d39009a6ccaec0c22f7bf
Sourena MOOSAVI <stloverflow@gmail.com>
2025-05-04 18:42:03 +0200

    binary_search.py is updated
    both binary search and recursive binary search are implemented
    in a more efficient way

--- binary_search.py
+++ binary_search.py
@@ -5,30 +5,21 @@ def binary_search(arr, ele):
 
     # Defining the first and last index values
     first = 0
-    last = len(arr)-1
-
-    found = False
-
+    last = len(arr) - 1
     
-    while first <= last and not found:
+    while first <= last:
 
         # Set the midpoint
-        mid = (first+last)//2
+        mid = (first+last) // 2
 
-        # Match found
-        if arr[mid] == ele:
-            found = True
-        
-        # Set new midpoints up or down depending on the comparison
+        if arr[mid] == ele: # Match found
+            return mid
+        elif ele < arr[mid]:
+            last = mid - 1
         else:
-            # Set down
-            if ele < arr[mid]:
-                last = mid-1
-            # Set up
-            else:
-                first = mid+1
-    
-    return found
+            first = mid + 1
+
+    return -1  # Element not found
 
 # Tasting our algorithm
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -37,32 +28,24 @@ print(binary_search(arr,13))
 print(binary_search(arr,4))
 
 
-
-def rec_bin_search(arr, ele):
+def rec_bin_search(arr, ele, first=0, last=None):
     """
     A recursive binary search algorithm"""
 
-    # Base case
-    if len(arr) == 0:
-        return False
+    if last is None:
+        last = len(arr) - 1
 
-    # Recursive case
+    if first > last:
+        return -1  # Element not found
+
+    mid = (first + last) // 2
+
+    if arr[mid] == ele:
+        return mid  # Return index
+    elif ele < arr[mid]:
+        return rec_bin_search(arr, ele, first, mid - 1)
     else:
-
-        mid = len(arr)//2
-
-        # If match found
-        if arr[mid] == ele:
-            return True
-        else:
-            
-            # Call again on second half
-            if ele < arr[mid]:
-                return rec_bin_search(arr[:mid],ele)
-            
-            # Or call on first half
-            else:
-                return rec_bin_search(arr[mid+1:], ele)
+        return rec_bin_search(arr, ele, mid + 1, last)
 
 # Tasting our algorithm
 print(rec_bin_search(arr, 19))
